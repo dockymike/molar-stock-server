debff9cbb9a554f1aace9da4e7f680db5f6f184e
dockymike <contactmichaelsimon@gmail.com>
2025-06-24 23:33:35 -0400

    updated db index.js for pooled connections

--- db/index.js
+++ db/index.js
@@ -6,13 +6,11 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // required for Heroku SSL
-  },
-  max: 5, // ✅ limit number of connections your app can open
-  idleTimeoutMillis: 10000, // ✅ close idle connections after 10 seconds
-  connectionTimeoutMillis: 2000, // ✅ fail if can't connect within 2 seconds
+  connectionString: process.env.DATABASE_URL_POOLED,
+  ssl: { rejectUnauthorized: false },
+  max: 1, // ✅ PgBouncer handles pooling now
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 2000,
 })
 
 export default pool
