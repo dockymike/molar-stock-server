af4989e59ebd5e16c5b9920d83aa70b38175ec23
dockymike <contactmichaelsimon@gmail.com>
2025-06-25 00:37:57 -0400

    fixed databaseurl in db/index.js

--- db/index.js
+++ db/index.js
@@ -6,9 +6,9 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_POOLED,
+  connectionString: process.env.DATABASE_URL, // ✅ Heroku uses this
   ssl: { rejectUnauthorized: false },
-  max: 1, // ✅ PgBouncer handles pooling now
+  max: 1, // ✅ PgBouncer handles pooling
   idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 2000,
 })
